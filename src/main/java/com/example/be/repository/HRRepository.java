f17e91c3d08a910059c35c7136e12faec8d42beb
Minh <phamminhheo2k5@gmail.com>
2025-09-22 17:14:13 +0700

    delete

--- src/main/java/com/example/be/repository/HRRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.be.repository;
-
-import com.example.be.entity.HR;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface HRRepository extends JpaRepository<HR, Long> {
-}
--- src/main/java/com/example/be/repository/InternProfileRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.be.repository;
-
-import com.example.be.entity.InternProfile;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface InternProfileRepository extends JpaRepository<InternProfile, Long> {
-}
