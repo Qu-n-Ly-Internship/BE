67d0629ede382619fbbbcbd33f555caf76feaad4
Minh <phamminhheo2k5@gmail.com>
2025-09-23 16:32:34 +0700

    minh

--- src/main/java/com/example/be/config/SecurityConfig.java
+++ src/main/java/com/example/be/config/SecurityConfig.java
@@ -42,7 +42,7 @@ public class SecurityConfig {
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:3000"));
+        config.setAllowedOrigins(List.of("http://localhost:5173"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE","PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
 
