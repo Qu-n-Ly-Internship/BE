d24eb82319bb0fe0a28f44761249db489cb72fc7
Minh <phamminhheo2k5@gmail.com>
2025-10-06 11:12:57 +0700

    đăng nhập gg

--- src/main/java/com/example/be/service/AuthService.java
+++ src/main/java/com/example/be/service/AuthService.java
@@ -135,12 +135,19 @@ public class AuthService {
 
         response.put("success", true);
         response.put("message", "Đăng nhập thành công!");
+        var permissions = user.getRole()
+                .getPermissions()
+                .stream()
+                .map(p -> p.getName())
+                .toList();
         response.put("user", Map.of(
                 "id", user.getId(),
                 "fullName", user.getFullName(),
                 "email", user.getEmail(),
+                "status", user.getStatus(),
                 "role", user.getRole().getName(),
-                "status", user.getStatus()));
+                "permissions", permissions));
+
         String token = jwtUtil.generateToken(request.getEmail(), user.getRole().getName());
         response.put("token", token);
         return response;
