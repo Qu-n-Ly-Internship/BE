09753f01230e13b9251a692ee158abcab6717bfc
Hckr2005 <phat14112005@gmail.com>
2025-11-19 09:59:54 +0700

    fix mentor assign

--- src/main/java/com/example/be/repository/InternProfileRepository.java
+++ src/main/java/com/example/be/repository/InternProfileRepository.java
@@ -2,6 +2,8 @@ package com.example.be.repository;
 
 import com.example.be.entity.InternProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,4 +13,13 @@ public interface InternProfileRepository extends JpaRepository<InternProfile, Lo
     Optional<InternProfile> findByUser_Id(Long internId);
 
     List<InternProfile> findByProgram_Id(Long programId);
+
+    @Query("""
+                SELECT ip FROM InternProfile ip
+                JOIN ip.program p
+                WHERE p.mentor.id = :mentorId
+                  AND ip.status = 'active'
+            """)
+    List<InternProfile> findActiveInternsByMentorId(@Param("mentorId") Long mentorId);
+
 }
